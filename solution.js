0900d43ef3c37b2f99596fd1e864b70b897ceed2
aatri8899 <aatri8899@withs.org>
2016-03-14 14:17:38 +0000

    updated jQuery Funhouse

--- solution.js
+++ solution.js
@@ -19,4 +19,19 @@
   return "#"+r+g+b;
  };
  
+ $(document).ready(function() {
+  $(function() {
+  $('#submit-name').click(function() {
+    window.greetUser($('#name').val());
+  });
+
+  $('.gallery-thumb').click(function() {
+    window.switchImage(this.src);
+  });
+
+  $('#change-color').click(function() {
+    window.changeBackgroundColor();
+  });
+});
+ });
  
